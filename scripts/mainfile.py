8657130509968a115fe952058ba8b64475c52c40
Jason <jrreynolds97@gmail.com>
2020-04-18 00:24:46 -0600

    Updated Mainfile to have basic structure and groups.

--- scripts/mainfile.py
+++ scripts/mainfile.py
@@ -1,16 +1,36 @@
 import sys, pygame
 
 if __name__ == "__main__":
+    # Basic initialization.
     pygame.init()
 
     running = True
     FPS = 30
     FPSClock = pygame.time.Clock()
 
-    test = 0
+    width = 640
+    height = 480
+    dimensions = (width, height)
 
+    viewport = pygame.display.set_mode(dimensions)
+    black = 0, 0, 0
+
+    # Groups for the sprites to fall into for mass updating.
+    G_Background = pygame.sprite.Group()
+    G_All = pygame.sprite.Group()
+    G_Player = pygame.sprite.Group()
+    G_Enemies = pygame.sprite.Group()
+    G_Neutral = pygame.sprite.Group()
+    # add more groups here as needed.
+
+    # Be very careful when adding things into this loop! This is the main run loop (limited by FPS).
     while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                sys.exit()
 
+        viewport.fill(black)
 
-
+        pygame.display.flip()
         FPSClock.tick(FPS)
